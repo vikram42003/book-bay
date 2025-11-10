e68d58fc895c79c322cbb38c8088e471344181f3
vikram(wsl) <vikramjit360@gmail.com>
2025-11-10 13:44:53 +0530

    feat: change fonts, use merriweather font for HomePage heading text

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import BooksGrid from "@/components/homepage/BooksGrid";
 export default function HomePage() {
   return (
     <div>
+      <h3 className="text-3xl font-merriweather text-center my-12 font-semibold text-blue-500">Showing All Books</h3>
       <BooksGrid />
     </div>
   );
